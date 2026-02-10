500f43ee9c60a2a96ed950d3c9e78e53f4888db4
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2026-02-10 17:05:17 +0200

    refactor NotificationManager to support Adventure API and legacy color codes for notifications

--- src/main/java/com/petarmc/petarlib/notifications/NotificationManager.java
+++ src/main/java/com/petarmc/petarlib/notifications/NotificationManager.java
@@ -1,33 +1,81 @@
 package com.petarmc.petarlib.notifications;
 
 
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
 import org.bukkit.entity.Player;
 
 public class NotificationManager {
 
+    // Adventure-based notifications (support minimessage)
+
     /**
      * Shows an action bar notification to the player. This will appear above the player's hotbar and disappear after a few seconds.
+     * Supports Adventure API Components ONLY!
      *
-     * @param msg
-     * @param p
+     * @param player The player to whom the action bar notification will be shown.
+     * @param component The component to show as an action bar notification.
      */
-    public void showActionBarNotif(String msg, Player p) {
-        p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(msg));
+    public void sendActionBarNotification(Player player, Component component) {
+        player.sendActionBar(component);
     }
 
     /**
-     * Sends a chat message to the player. This is not an action bar notification, but a regular chat message.
+     * Shows a chat message to the player.
+     * Supports Adventure API Components ONLY!
      *
-     * @param msg
-     * @param p
+     * @param player The player to whom the chat message will be sent.
+     * @param component The component to send as a chat message.
      */
-    public void sendChatNotif(String msg, Player p) {
-        p.sendMessage(msg);
+    public void sendChatNotification(Player player, Component component) {
+        player.sendMessage(component);
+    }
+
+    /**
+     * Shows a title notification to the player. This will appear in the center of the player's screen and can include a title, subtitle, and fade in/out times.
+     * Supports Adventure API Title objects ONLY!
+     *
+     * @param player
+     * @param title
+     */
+    public void sendTitleNotification(Player player, Title title) {
+        player.showTitle(title);
     }
 
 
+    // Legacy color codes based notifications
 
+    /**
+     * Shows an action bar notification to the player. This will appear above the player's hotbar and disappear after a few seconds.
+     * Supports legacy color codes (e.g. &c for red) and formatting codes.
+     *
+     * @param player The player to whom the action bar notification will be shown.
+     * @param message The message to show as an action bar notification, can use legacy color.
+     */
+    public void sendActionBarNotificationLegacy(Player player, String message) {
+        player.sendActionBar(message);
+    }
 
+    /**
+     * Shows a chat message to the player.
+     * Supports legacy color codes (e.g. &c for red) and formatting codes.
+     *
+     * @param player The player to whom the chat message will be sent.
+     * @param message The message to send as a chat message, can use legacy color.
+     */
+    public void sendChatNotificationLegacy(Player player, String message) {
+        player.sendMessage(message);
+    }
+
+    /**
+     * Shows a title notification to the player. This will appear in the center of the player's screen.
+     * Supports legacy color codes (e.g. &c for red) and formatting codes.
+     *
+     * @param player The player to whom the title notification will be shown.
+     * @param title The title text to show, can use legacy color.
+     * @param subtitle The subtitle text to show, can use legacy color.
+     */
+    public void sendTitleNotificationLegacy(Player player, String title, String subtitle) {
+        player.sendTitle(title, subtitle);
+    }
 }
