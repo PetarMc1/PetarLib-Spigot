1436e3050da01e2006d083d0950bea4e87063810
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2026-02-10 16:39:11 +0200

    remove bukkit/spigot support

--- src/main/java/com/petarmc/petarlib/PetarLib.java
+++ src/main/java/com/petarmc/petarlib/PetarLib.java
@@ -4,34 +4,19 @@ import com.petarmc.petarlib.commands.PetarLibCommand;
 import com.petarmc.petarlib.expansion.PlaceHolderAPIExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import org.jspecify.annotations.NonNull;
-
 import java.util.Objects;
 
 public final class PetarLib extends JavaPlugin {
     private static PetarLib plugin;
-    private BukkitAudiences adventure;
     private  boolean isPlaceHolderAPIActive = false;
     public static boolean DebugMode = false;
     public static PetarLib getPlugin() {
         return plugin;
     }
 
-    //used for adventure
-    @NonNull
-    public BukkitAudiences adventure() {
-        if (this.adventure == null) {
-            throw new IllegalStateException("Tried to access Adventure when the plugin was disabled!");
-        }
-        return this.adventure;
-    }
-
     @Override
     public void onEnable() {
         plugin = this;
-        //used for adventure
-        this.adventure = BukkitAudiences.create(this);
 
         saveDefaultConfig();
         DebugMode = getConfig().getBoolean("debug", false);
@@ -61,11 +46,5 @@ public final class PetarLib extends JavaPlugin {
     public void onDisable() {
         getLogger().info("PetarLib stopped successfully!");
 
-
-        //used for adventure
-        if (this.adventure != null) {
-            this.adventure.close();
-            this.adventure = null;
-        }
     }
 }
