c9ecc340011a91eb072b4b09a842c52fa88c9d8b
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2026-02-12 19:07:02 +0200

    Add /petarlib send command

--- src/main/java/com/petarmc/petarlib/commands/PetarLibCommand.java
+++ src/main/java/com/petarmc/petarlib/commands/PetarLibCommand.java
@@ -1,9 +1,11 @@
 package com.petarmc.petarlib.commands;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.Locale;
 
@@ -11,6 +13,35 @@ import static com.petarmc.petarlib.PetarLib.getPlugin;
 
 
 public class PetarLibCommand implements CommandExecutor {
+    // notifications function that is used by the /petarlib send <player> <message> command.
+    MiniMessage mm = MiniMessage.miniMessage();
+    private enum Type {
+        ACTIONBAR,
+        CHAT,
+        TITLE
+    }
+    private void petarLibSendCmd(String text, Player player, Type type) {
+        if (getPlugin().getConfig().getBoolean("use-minimessage")){
+            switch (type) {
+                case ACTIONBAR:
+                    player.sendActionBar(mm.deserialize(text));
+                    break;
+                case CHAT:
+                    player.sendMessage(mm.deserialize(text));
+                    break;
+            }
+        } else {
+            switch (type) {
+                case ACTIONBAR:
+                    player.sendActionBar(text);
+                    break;
+                case CHAT:
+                    player.sendMessage(text);
+                    break;
+            }
+        }
+
+    }
     @Override
     public boolean onCommand( CommandSender sender,  Command command,  String label,  String[] args) {
         String cmd = command.getName();
@@ -36,15 +67,37 @@ public class PetarLibCommand implements CommandExecutor {
                 break;
             case "reload":
                 if (!sender.hasPermission("petarlib.admin")) {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+                    sender.sendMessage("You don't have permission to use this command.");
                     return true;
                 }
                 getPlugin().reloadConfig();
                 sender.sendMessage(ChatColor.GREEN + "PetarLib configuration reloaded.");
                 break;
+            case "send":
+                if (args.length < 4) {
+                    sender.sendMessage("Usage: /petarlib send <player> <message> <type>");
+                    return true;
+                }
+                if (!sender.hasPermission("petarlib.send")) {
+                    sender.sendMessage("You don't have permission to use this command.");
+                    return true;
+                }
+                Player target = getPlugin().getServer().getPlayer(args[1]);
+                Type type = args[3].equalsIgnoreCase("actionbar") ? Type.ACTIONBAR : args[3].equalsIgnoreCase("chat") ? Type.CHAT : args[3].equalsIgnoreCase("title") ? Type.TITLE : null;
+                if (target == null) {
+                    sender.sendMessage("Player not found.");
+                    return true;
+                }
+                if (type == null) {
+                    sender.sendMessage("Invalid type. Use 'actionbar' or 'chat'.");
+                    return true;
+                }
+                String message = String.join(" ", args).substring(args[0].length() + args[1].length() + 2);
+                petarLibSendCmd(message, target, type);
+                break;
             case "debug":
                 if (!sender.hasPermission("petarlib.admin")) {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+                    sender.sendMessage("You don't have permission to use this command.");
                     return true;
                 }
                 boolean currentDebug = getPlugin().getConfig().getBoolean("debug");
@@ -57,10 +110,9 @@ public class PetarLibCommand implements CommandExecutor {
                 }
                 break;
             default:
-                sender.sendMessage(ChatColor.RED + "Unknown subcommand. Use /petarlib help");
+                sender.sendMessage("Unknown subcommand. Use /petarlib help");
                 break;
         }
-
         return true;
     }
 
